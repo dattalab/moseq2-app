0bf0efa0c1f0e218ee7aae59ecc55f6b2926e2d8
Ayman Zeine <a.zeine.96@gmail.com>
2019-09-05 21:23:43 -0400

    Fully implemented config and extract + additional formatting

--- modules/app/controllers/extract-controller.py
+++ modules/app/controllers/extract-controller.py
@@ -1,9 +1,15 @@
 import os
 import glob
 from flask import request, jsonify
-from app import app, data_path#, mongo
+from app import app, data_path, data_config#, mongo
 import logger
 import subprocess
+from ast import literal_eval
+from moseq2_extract.gui import *
+import pickle
+
+
+global config_dict
 
 @app.route('/get-local-dir', methods=['GET'])
 def check_local_data_dir():
@@ -28,18 +34,33 @@ def extract_generate_conf():
         cd_cmd = 'cd ' + cwd
         os.system(cd_cmd)
         data = request.form.to_dict()
-        print(data)
 
-        query = request.args
-
-        if query == {}:
+        if data == {}:
             os.system(f'moseq2-extract generate-config -o {cwd1}/config.yaml')
             if os.path.exists(cwd1+'config.yaml'):
                 return jsonify({'ok': True, 'message': cwd1+'/config.yaml'}), 200
 
-        # if yes, send json warning, if accepted will receive a future PATCH request
+        else:
+            for k, v in data.items():
+                if ',' in v:
+                    data[k] = literal_eval('('+v+')')
+                elif v == 'on':
+                    data[k] = True
+                elif v == 'off':
+                    data[k] = False
+                elif v.isdigit() or '-' in v:
+                    data[k] = int(v)
 
-        # if no, runs config file creation, returns success message and path to generated file
+            data['flip_classifier'] = ''
+            data['config_file'] = cwd1+'config.yaml'
+
+            generate_config_command(cwd1+'config.yaml', data)
+            with open(cwd+data_config+'param_data.pkl', 'wb') as handle:
+                pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+            #os.system(f'moseq2-extract generate-config -o {cwd1}config.yaml -g {data}')
+            if os.path.exists(cwd1+'config.yaml'):
+                return jsonify({'ok': True, 'message': cwd1+'/config.yaml'}), 200
 
         return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
@@ -47,28 +68,23 @@ def extract_generate_conf():
 def batch_gen_gridsearch_conf(path=None):
     return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
-@app.route('/extract-raw', methods=['GET'])
-def extract_raw(path=None):
+@app.route('/extract-raw', methods=['POST'])
+def extract_raw():
 
     cwd = os.getcwd()
     cwd1 = cwd + data_path
 
-    if request.method == 'GET':
+    if request.method == 'POST':
         #start cli command with default params unless get dict is not empty
         cd_cmd = 'cd '+cwd
         os.system(cd_cmd)
 
-        query = request.args
+        input_files = request.form.to_dict()
 
-        if query == {}:
+        if input_files == {}:
             # run extract with default cmd
             filepath = cwd1+'depth.dat'
             configfile = 'config.yaml'
-            '''
-            extract(filepath, (80, 80), (10, 10), 'ellipse', 0, (1, .1, 1), (650, 750), False, 3000, 7, True, 10, 100,
-                    30, None, 51, False, 100, -16, -100, True, 'raw', 0, 'rectangle', (5, 5), 1, (9, 9), 'ellipse', [3],
-                    [0], 1000, 0, None, True, True, 0, 3, 0, 3, (0, 0), (0, 0), configfile, False, 3000, 3)
-            '''
 
             os.system(f'moseq2-extract extract {filepath}')
 
@@ -79,11 +95,28 @@ def extract_raw(path=None):
             else:
                 return jsonify({'ok': True, 'message': 'Extraction not successful'}), 400
         else:
-            # run extract with user defined params
-            if len(os.listdir(cwd1)) > 3:
+            with open(cwd+data_config+'param_data.pkl', 'rb') as handle:
+                data = pickle.load(handle)
+
+            # Update to support multiple files
+            extract_command(cwd1+input_files['depth-file'], data['crop_size'], data['bg_roi_dilate'], data['bg_roi_shape'], data['bg_roi_index'], data['bg_roi_weights'],
+                            data['bg_roi_depth_range'], data['bg_roi_gradient_filter'], data['bg_roi_gradient_threshold'], data['bg_roi_gradient_kernel'], data['bg_roi_fill_holes'],
+                            data['min_height'], data['max_height'], data['fps'], data['flip_classifier'], data['flip_classifier_smoothing'],data['use_tracking_model'], 
+                            data['tracking_model_ll_threshold'], data['tracking_model_mask_threshold'], data['tracking_model_ll_clip'], data['tracking_model_segment'],
+                            data['tracking_model_init'], data['cable_filter_iters'], data['cable_filter_shape'], data['cable_filter_size'], data['tail_filter_iters'],
+                            data['tail_filter_size'], data['tail_filter_shape'], [data['spatial_filter_size']], [data['temporal_filter_size']], data['chunk_size'], data['chunk_overlap'],
+                            cwd1+'proc/', data['write_movie'], data['use_plane_bground'], 'uint8', data['centroid_hampel_span'], data['centroid_hampel_sig'],
+                            data['angle_hampel_span'], data['angle_hampel_sig'], data['model_smoothing_clips'], data['frame_trim'], data['config_file'], 
+                            data['compress'], data['chunk_size'], data['num_compress_threads'])
+
+            if os.path.exists(cwd1+'proc/'):
+                os.system(f'cp {cwd1}proc/bground.tiff {cwd}/modules/app/static/output_imgs/bground.png')
+                os.system(f'cp {cwd1}proc/first_frame.tiff {cwd}/modules/app/static/output_imgs/first_frame.png')
+                os.system(f'cp {cwd1}proc/roi_00.tiff {cwd}/modules/app/static/output_imgs/roi_00.png')
+                os.system(f'cp {cwd1}proc/results_00.mp4 {cwd}/modules/app/static/output_imgs/results_00.mp4')
+
                 return jsonify({'ok': True, 'message': 'Extraction successful'}), 200
-            else:
-                return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
+        return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
 @app.route('/extract-batch', methods=['GET', 'POST', 'DELETE', 'PATCH'])
 def batch_extract_raw(path=None):
--- modules/app/static/js/extract_script.js
+++ modules/app/static/js/extract_script.js
@@ -5,15 +5,63 @@ $(document).onchange(function() {
 var benchFiles = []
 var openNavBool = false;
 
+window.onload = function() {
+
+}
+
+
+function loadImageModal(event) {
+
+    // Get the modal
+    var modal = document.getElementById("imageModal");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById(event.target.id);
+
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      captionText.innerHTML = this.alt;
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
 function getLocalDir() {
     $.get('/get-local-dir', {}, function(resp) {
         if (resp.ok) {
             // change button color to green and display extract button
             var fileList = resp.files;
             benchFiles = fileList;
+            var e = document.querySelector("#filelist");
+            var child = e.lastElementChild;
+            while (child) {
+                e.removeChild(child);
+                child = e.lastElementChild;
+            }
+            var f = document.querySelector("#extractedlist");
+            var child = f.lastElementChild;
+            while (child) {
+                f.removeChild(child);
+                child = f.lastElementChild;
+            }
             for(var i = 0; i < fileList.length; i++) {
-                $('#filelist').append('<option>'+fileList[i]+'</option>');
-                if(fileList[i] == 'session1/depth.dat') {
+                $('#filelist').append('<option onclick="selectFile(event)">'+fileList[i]+'</option>');
+                if(fileList[i] == 'sample1/depth.dat') {
                     $('#extractedlist').append('<option>'+fileList[i]+'</option>');
                 }
             }
@@ -45,8 +93,6 @@ function uploadExParams() {
         })
         .then((resp) => {
             // Done: inform user that upload is finished.
-            //Change to sessions
-            //selDiv.innerHTML += "Total number of files identified: " + files.length + " <br/>"
             console.log('config successfully generated');
 
         })
@@ -57,21 +103,82 @@ function uploadExParams() {
 }
 
 function extractRaw() {
-    $.get('/extract-raw', {}, function(resp) {
-        if (resp.ok) {
-            alert(resp.message);
-            window.location.reload();
-        } else {
-            alert(resp.message);
-        }
+    var url = '/extract-raw'
+    var formData = new FormData();
+
+    var ul = document.getElementById("");
+
+    $('#selected-list li').each(function(i)
+    {
+       var filename = $(this).attr('id');
+       filename = filename.replace('-', '/');
+
+       formData.append('depth-file', filename);
     });
+
+
+    fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then((resp) => {
+            // Done: inform user that upload is finished.
+            //Change to sessions
+            //selDiv.innerHTML += "Total number of files identified: " + files.length + " <br/>"
+            var photos = ['/static/output_imgs/bground.tiff', '/static/output_imgs/roi_00.tiff', '/static/output_imgs/first_frame.tiff', '/static/output_imgs/results_00.mp4'];
+            var alts = ['Background Image', 'ROI Image', 'First Frame', 'Extracted Video'];
+            var gallery = document.getElementById('gal');
+            for(var i = 0; i < photos.length; i++) {
+                var figure = document.createElement("figure");
+                figure.setAttribute('class','gallery__item');
+
+                if(i < photos.length-1) {
+                    var img = document.createElement("img");
+                    img.setAttribute('class', "gallery__img");
+                    img.setAttribute('src', photos[i]);
+                    img.setAttribute('id', "img"+i);
+                    img.setAttribute('type', 'image/tiff');
+                    img.setAttribute('alt', alts[i]);
+                    figure.appendChild(img)
+                }
+                else {
+                    var vid = document.createElement("video");
+                    vid.setAttribute('class', "gallery__img");
+                    vid.setAttribute('alt', alts[i]);
+                    vid.setAttribute('id', "vid"+i);
+                    var source = document.createElement("source");
+                    source.setAttribute('src', photos[i]);
+                    source.setAttribute('type', 'video/mp4');
+                    vid.appendChild(source);
+                    figure.appendChild(vid)
+                }
+                gallery.appendChild(figure);
+            }
+            console.log('extraction successfully performed');
+
+        })
+        .catch(() => {
+            console.log('error :(');
+            // Error: inform user of upload error response.
+    });
+
 }
 
 function findROI() {
     $.get('/find-roi', {}, function(resp) {
         if (resp.ok) {
             alert(resp.message);
-            window.location.reload();
+        } else {
+            alert(resp.message);
+        }
+    });
+}
+
+function copySlices() {
+    $.get('/copy-slice', {}, function(resp) {
+        if (resp.ok) {
+            alert(resp.message);
+
         } else {
             alert(resp.message);
         }
@@ -143,6 +250,7 @@ function toggleBatchModeExtract(evt) {
 
 function openNav() {
   if (!openNavBool) {
+      getLocalDir();
       document.getElementById("mySidebar").style.width = "300px";
       document.getElementById("extract-tab").style.marginRight = "300px";
       openNavBool = !openNavBool;
@@ -175,5 +283,23 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
     if (evt.currentTarget.id == 'init-step-button') {
         getLocalDir();
+    }
+}
+
+function selectFile(event) {
+    var clickedOpt = event.target;
+    var clickedOptName = event.target.innerHTML;
+    var ul = document.getElementById("selected-list");
+    var clickedOptId = clickedOptName.replace('/','-');
+    //clickedOptId = clickedOptId.replace('.', '_');
+
+    if (clickedOpt.selected) {
+        var li = document.createElement("li");
+        li.setAttribute('id',clickedOptId);
+        li.appendChild(document.createTextNode(clickedOptName));
+        ul.appendChild(li);
+    } else {
+        var item = document.getElementById(clickedOptId);
+        ul.removeChild(item);
     }
 }
