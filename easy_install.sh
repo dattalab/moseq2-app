3be0535bbb1765992e02cb8b5e9572dcad5bc84a
Ayman Zeine <a.zeine.96@gmail.com>
2020-01-30 13:08:23 -0500

    Updated install branches to 'release'

--- easy_install.sh
+++ easy_install.sh
@@ -3,6 +3,6 @@ conda env create -f moseq2-env.yaml
 conda activate moseq2dev
 export CC=/usr/local/bin/gcc-7
 export CXX=/usr/local/bin/g++-7
-pip install git+https://github.com/dattalab/moseq2-model.git@dev
+pip install git+https://github.com/dattalab/moseq2-model.git@release
 jupyter contrib nbextension install --user
 jupyter nbextensions_configurator enable --user
