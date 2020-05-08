e9faa436e60f5444398f8f3daf23d3966577afb1
Ayman Zeine <a.zeine.96@gmail.com>
2020-05-08 11:12:07 -0400

    easy_install gcc install fix

--- easy_install.sh
+++ easy_install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 conda env create -f moseq2-env.yaml
-conda activate moseq2dev
-./install_gcc.sh
+conda init
+conda activate moseq2
+read -p "Would you like to try auto-installing GCC-7? [y/n]" y_gcc
+stry="y"
+if [ "$y_gcc" == "$stry" ]; then
+  {
+    ./install_gcc.sh
+  }
+fi
 pip install git+https://github.com/dattalab/moseq2-model.git@release
 jupyter contrib nbextension install --user
 jupyter nbextensions_configurator enable --user
