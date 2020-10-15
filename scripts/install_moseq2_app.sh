2ccfc8521155801b4c7051d4aa90cf5a3324e36f
Ayman Zeine <a.zeine.96@gmail.com>
2020-10-15 14:02:30 -0400

    Installation Bug Fixes

--- scripts/install_moseq2_app.sh
+++ scripts/install_moseq2_app.sh
@@ -3,12 +3,13 @@
 pip install -e .
 
 # Ensure latest version of jupyter is installed
-pip install --upgrade jupyter
+pip install --upgrade jupyter requests
 
 # Ensure ffmpeg is installed
-conda install -c conda-forge ffmpeg -y
+conda install -c conda-forge ffmpeg=4.2.0 -y
 
 # Install and Enable widget extensions configurator
+conda install -c conda-forge jupyter_nbextensions_configurator
 jupyter nbextension install --py jupyter_nbextensions_configurator --sys-prefix
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
