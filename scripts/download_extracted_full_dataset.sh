626500fbb8fc4ffb5af47e2054d9ba93e456f7a4
Ayman Zeine <a.zeine.96@gmail.com>
2020-11-17 17:56:10 -0500

    Update download_extracted_full_dataset.sh

--- scripts/download_extracted_full_dataset.sh
+++ scripts/download_extracted_full_dataset.sh
@@ -105,14 +105,14 @@ curl -L -o saline_example_23.zip https://www.dropbox.com/sh/ggs7zcsj5mmtcua/AABl
 rm saline_example_23.zip
 
 # PCA Files
-curl -L -o _pca.zip https://www.dropbox.com/sh/m4fw0uuoqzhnk3q/AAA97ol_QiT2YGXto50hvWpUa?dl=0 && unzip _pca.zip -d _pca
+curl -L -o _pca.zip https://www.dropbox.com/sh/ht36mw2a04h0a1r/AAAjMIFTFva4zr7vs_JavyZva?dl=0 && unzip _pca.zip -d _pca
 rm _pca.zip
 
 # Modeling Files
 curl -L -o saline-amphetamine.zip https://www.dropbox.com/sh/qnsrbpqrbot0lw2/AAAB3HBUGLVAY1HxWztY6RKla?dl=0  && unzip saline-amphetamine.zip -d saline-amphetamine
 rm saline-amphetamine.zip
 
-# Progress and Index Files
+# Progress File
 curl -L -o progress.yaml https://www.dropbox.com/s/49445asfydjpkvd/progress.yaml?dl=0
 
 # Config File
