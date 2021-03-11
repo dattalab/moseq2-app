ae3ca00a5b14d90961268f96412ca8e2d7bf853d
Ayman Zeine <a.zeine.96@gmail.com>
2021-03-11 13:35:11 -0500

    Updated GUI widgets + wrappers

--- moseq2_app/gui/wrappers.py
+++ moseq2_app/gui/wrappers.py
@@ -6,15 +6,11 @@ main.py.
 '''
 
 import os
-import qgrid
 import shutil
-import pandas as pd
-import ruamel.yaml as yaml
 import ipywidgets as widgets
 from bokeh.io import show
 from moseq2_viz.util import read_yaml
 from ipywidgets import interactive_output
-from moseq2_app.util import index_to_dataframe
 from IPython.display import display, clear_output
 from moseq2_app.flip.controller import FlipRangeTool
 from moseq2_app.gui.progress import get_session_paths
@@ -116,73 +112,18 @@ def interactive_group_setting_wrapper(index_filepath):
     -------
     '''
 
-    index_grid = GroupSettingWidgets()
-
-    index_dict, df = index_to_dataframe(index_filepath)
-    qgrid_widget = qgrid.show_grid(df[['SessionName', 'SubjectName', 'group', 'uuid']], column_options=index_grid.col_opts,
-                                   column_definitions=index_grid.col_defs, show_toolbar=False)
-
-    def update_table(b):
-        '''
-        Updates table upon "Set Button" click
-
-        Parameters
-        ----------
-        b (button click)
-
-        Returns
-        -------
-        '''
-
-        index_grid.update_index_button.button_style = 'info'
-        index_grid.update_index_button.icon = 'none'
-
-        selected_rows = qgrid_widget.get_selected_df()
-        x = selected_rows.index
-
-        for i in x:
-            qgrid_widget.edit_cell(i, 'group', index_grid.group_input.value)
-
-    def update_clicked(b):
-        '''
-        Updates the index file with the current table state upon Save button click.
-
-        Parameters
-        ----------
-        b (button click)
-
-        Returns
-        -------
-        '''
-
-        files = index_dict['files']
-        meta = [f['metadata'] for f in files]
-        meta_cols = pd.DataFrame(meta).columns
-
-        latest_df = qgrid_widget.get_changed_df()
-        df.update(latest_df)
-
-        updated_index = {'files': list(df.drop(meta_cols, axis=1).to_dict(orient='index').values()),
-                         'pca_path': index_dict['pca_path']}
-
-        with open(index_filepath, 'w+') as f:
-            yaml.safe_dump(updated_index, f)
-
-        index_grid.update_index_button.button_style = 'success'
-        index_grid.update_index_button.icon = 'check'
-
-    def clear_clicked(b):
-        # Clear the display
-        clear_output()
+    index_grid = GroupSettingWidgets(index_filepath)
 
     # Add callback functions
-    index_grid.clear_button.on_click(clear_clicked)
-    index_grid.update_index_button.on_click(update_clicked)
-    index_grid.save_button.on_click(update_table)
+    index_grid.clear_button.on_click(index_grid.clear_clicked)
+    index_grid.update_index_button.on_click(index_grid.update_clicked)
+    index_grid.save_button.on_click(index_grid.update_table)
 
     # Display output
     display(index_grid.clear_button, index_grid.group_set)
-    display(qgrid_widget)
+    display(index_grid.qgrid_widget)
+
+    return index_grid
 
 def interactive_syllable_labeler_wrapper(model_path, config_file, index_file, crowd_movie_dir, output_file,
                                          max_syllables=None, n_explained=99):
@@ -199,9 +140,6 @@ def interactive_syllable_labeler_wrapper(model_path, config_file, index_file, cr
     Returns
     -------
     '''
-
-    # Load the config file
-    config_data = read_yaml(config_file)
 
     # Copy index file to modeling session directory
     modeling_session_dir = os.path.dirname(model_path)
@@ -225,14 +163,10 @@ def interactive_syllable_labeler_wrapper(model_path, config_file, index_file, cr
     labeler = SyllableLabeler(model_fit=model,
                               model_path=model_path,
                               index_file=index_file,
+                              config_file=config_file,
                               max_sylls=max_sylls,
+                              crowd_movie_dir=crowd_movie_dir,
                               save_path=output_file)
-
-    # Populate syllable info dict with relevant syllable information
-    labeler.get_crowd_movie_paths(index_file, model_path, config_data, crowd_movie_dir)
-
-    # Set the syllable dropdown options
-    labeler.syll_select.options = labeler.syll_info
 
     # Launch and display interactive API
     output = widgets.interactive_output(labeler.interactive_syllable_labeler, {'syllables': labeler.syll_select})
