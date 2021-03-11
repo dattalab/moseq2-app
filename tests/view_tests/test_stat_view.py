51983769471122b5602c662a103ec725f6b906d3
Ayman Zeine <a.zeine.96@gmail.com>
2021-03-11 16:34:48 -0500

    Added Stat View Tests

--- tests/view_tests/test_stat_view.py
+++ tests/view_tests/test_stat_view.py
@@ -1,4 +1,7 @@
+import numpy as np
 from unittest import TestCase
+from bokeh.plotting import figure
+from moseq2_app.stat.view import clamp, colorscale, format_graphs, get_difference_legend_items
 
 class TestStatView(TestCase):
 
@@ -6,18 +9,46 @@ class TestStatView(TestCase):
         pass
 
     def test_clamp(self):
-        pass
+
+        val1 = clamp(-10)
+        assert val1 == 0
 
     def test_colorscale(self):
-        pass
+        ret = colorscale('#111111', -1)
+        assert ret == '111111'
 
-    def test_draw_stats(self):
-        pass
+    def test_format_graphs(self):
+
+        graphs = [ 'a', 'b', 'c',  'b-a', 'c-b', 'c-a' ]
+
+        group = ['a', 'b', 'c']
+
+        output = format_graphs(graphs, group)
+
+        expected_output = np.array([
+                            np.array(['a', 'b-a', 'c-b'], dtype=object),
+                            np.array([None, 'b', 'c-a'], dtype=object),
+                            np.array([None, None, 'c'], dtype=object)
+        ])
+
+        for i in range(3):
+            assert all(output[i] == expected_output[i])
+
+    def test_get_difference_legend_items(self):
+
+        fig = figure()
+
+        edge_widths = {i: x for i, x in enumerate(range(10, 1))}
+
+        diff_main_items, diff_width_items = get_difference_legend_items(fig, edge_widths, 'default')
+
+        assert len(diff_main_items) == 4
+        assert len(diff_width_items) == 4
 
     def test_bokeh_plotting(self):
         pass
 
-    def test_format_graphs(self):
+    def test_draw_stats(self):
         pass
 
     def test_get_neighbors_and_entropies(self):
