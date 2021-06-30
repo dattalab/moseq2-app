aa27ac8aca96c8c349b9f29bd5359abb7c7e8b23
Ayman Zeine <a.zeine.96@gmail.com>
2021-06-30 17:54:10 -0400

    fix: Copying all the notebooks in the download script [ci skip]

--- scripts/download_extracted_full_dataset.sh
+++ scripts/download_extracted_full_dataset.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mkdir full_extracted_test_ds
-cp notebooks/Main-MoSeq2-Notebook.ipynb full_extracted_test_ds
-cp notebooks/Interactive-Model-Results-Exploration.ipynb full_extracted_test_ds
+cp notebooks/* full_extracted_test_ds
 cd full_extracted_test_ds
 
 # amphetamine dataset
