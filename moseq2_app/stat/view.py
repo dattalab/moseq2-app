2b7498d2be91e0b83fb415d43d128d2574fe99c9
Sherry <sherrylin42@gmail.com>
2021-07-17 10:23:09 -0400

    dev print [ci skip]

--- moseq2_app/stat/view.py
+++ moseq2_app/stat/view.py
@@ -367,6 +367,7 @@ def get_syllable_info(df, sorting):
         except ValueError:
             # cm path does not exist
             cm_paths.append('')
+    print('cm path is', cm_paths)
 
     return labels, desc, cm_paths
 
@@ -1124,6 +1125,7 @@ def get_group_node_syllable_info(syll_info, node_indices):
         except ValueError:
             # crowd movie path not found
             cm_paths.append('')
+    print('cm path is', cm_paths)
 
     return labels, descs, cm_paths
 
