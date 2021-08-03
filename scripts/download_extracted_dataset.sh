30fa86231cf319369568b868a45e820d2c72887a
Sherry <sherrylin42@gmail.com>
2021-08-03 12:06:57 -0400

    feat: add colab compatible extracted dataset

---
+++ scripts/download_extracted_dataset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir test_data
+cd test_data
+
+# Progress File
+curl -L -o progress.yaml https://www.dropbox.com/s/2zayp2gbz5e4ssf/config_colab.yaml?dl=0
+
+# Config File
+curl -L -o config.yaml https://www.dropbox.com/s/i51yfglirupi32h/progress_colab.yaml?dl=0
+
+# Index File
+curl -L -o moseq2-index.yaml https://www.dropbox.com/s/iirgxqec2rp65nl/moseq2-index_colab.yaml?dl=0
+
+# Aggregate Sessions
+curl -L -o -o aggregage_results.zip https://www.dropbox.com/sh/fdi8mq4a2s4xp9q/AABlU230uGutBI6oA7QhrMkOa?dl=0 && unzip aggregage_results.zip -d aggregate_results
+rm aggregage_results.zip
