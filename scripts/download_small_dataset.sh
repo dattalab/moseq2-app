2068bb2c712e233aabd90ba5abd18140fcb3f20a
Sherry <sherrylin42@gmail.com>
2021-08-03 10:24:12 -0400

    chore: shrink test data size

--- scripts/download_small_dataset.sh
+++ scripts/download_small_dataset.sh
@@ -7,25 +7,9 @@ curl -L -o amphetamine_example_0.zip https://www.dropbox.com/sh/gnx4htuvmb7ue65/
 rm amphetamine_example_0.zip
 curl -L -o amphetamine_example_1.zip https://www.dropbox.com/sh/e2o8v9skrb0ef6b/AAA0y6no4eNPHzqMxd-eQqYxa?dl=1 && unzip amphetamine_example_1.zip -d amphetamine_example_1
 rm amphetamine_example_1.zip
-curl -L -o amphetamine_example_2.zip https://www.dropbox.com/sh/k7ieoducleexbpq/AAAQKOAjKt1k1nU2L5OzYsNsa?dl=1 && unzip amphetamine_example_2.zip -d amphetamine_example_2 
-rm amphetamine_example_2.zip
-curl -L -o amphetamine_example_3.zip https://www.dropbox.com/sh/yywypn9m6z1lpyt/AABAPaoEJugycnLNI0ICNnnXa?dl=1 && unzip amphetamine_example_3.zip -d amphetamine_example_3 
-rm amphetamine_example_3.zip
-curl -L -o amphetamine_example_4.zip https://www.dropbox.com/sh/0y39kqswphvemuq/AADfkxAEq5tJtwZIY8YgqNaza?dl=1 && unzip amphetamine_example_4.zip -d amphetamine_example_4 
-rm amphetamine_example_4.zip
-curl -L -o amphetamine_example_5.zip https://www.dropbox.com/sh/vrqvg575ak84mjn/AAAd7lWMcawniKuuV_uHgRqga?dl=1 && unzip amphetamine_example_5.zip -d amphetamine_example_5 
-rm amphetamine_example_5.zip
 
 # saline data
 curl -L -o saline_example_0.zip https://www.dropbox.com/sh/ca9nbj2vlsn8hyq/AACuBEODDwaizuITN6DjXv39a?dl=1 && unzip saline_example_0.zip -d saline_example_0
 rm saline_example_0.zip
 curl -L -o saline_example_1.zip https://www.dropbox.com/sh/87yzfv07d6tcrre/AAB0qXcgzGf-vRoTove34GGRa?dl=1 && unzip saline_example_1.zip -d saline_example_1
 rm saline_example_1.zip
-curl -L -o saline_example_2.zip https://www.dropbox.com/sh/u3hhekkzeejk0a5/AAAjt2WNDbX3U-G80IVDkahBa?dl=1 && unzip saline_example_2.zip -d saline_example_2
-rm saline_example_2.zip
-curl -L -o saline_example_3.zip https://www.dropbox.com/sh/a71pzlze1sbvuq3/AADTKZ_9LAzrQ3xSeJk_1GdMa?dl=1 && unzip saline_example_3.zip -d saline_example_3
-rm saline_example_3.zip
-curl -L -o saline_example_4.zip https://www.dropbox.com/sh/91gsub2ok6thnzo/AADSKuQuKSjQEcuT1SDlsICja?dl=1 && unzip saline_example_4.zip -d saline_example_4
-rm saline_example_4.zip
-curl -L -o saline_example_5.zip https://www.dropbox.com/sh/ogolgcyg7wvr9sj/AAD6Mwni4rMgsnDvnmst626na?dl=1 && unzip saline_example_5.zip -d saline_example_5
-rm saline_example_5.zip
