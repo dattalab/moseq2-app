f22ec25a2b819c06f64cdeff3e3d4b46554c9b4d
Ayman Zeine <a.zeine.96@gmail.com>
2021-08-16 17:45:00 -0400

    fix: sig-syll order mapping is reversed.

--- moseq2_app/stat/controller.py
+++ moseq2_app/stat/controller.py
@@ -311,7 +311,7 @@ class InteractiveSyllableStats(SyllableStatWidgets):
                 sig_sylls_indices = self.run_selected_hypothesis_test(hyp_test, stat, ctrl_group, exp_group)
 
                 # renumber the significant syllables s.t. they are plotted to match the current ordering.
-                ordermapping = {i: o for i, o in enumerate(ordering)}
+                ordermapping = {o: i for i, o in enumerate(ordering)}
                 sig_sylls = [ordermapping[x] for x in sig_sylls_indices]
 
         elif sort.lower() == 'similarity':
