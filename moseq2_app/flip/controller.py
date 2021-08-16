f374d5c1031d747dfce70f5d1230aa04b0c6fc1f
Ayman Zeine <a.zeine.96@gmail.com>
2021-08-15 22:45:08 -0400

    docs: changed "flipped" to "oriented". [ci skip]

--- moseq2_app/flip/controller.py
+++ moseq2_app/flip/controller.py
@@ -218,7 +218,7 @@ class FlipRangeTool(FlipClassifierWidgets):
         -------
         '''
 
-        cols = ['0 - Correctly Flipped (Facing East)', '1 - Incorrectly Flipped (Facing West)']
+        cols = ['0 - Correctly Oriented (Facing East)', '1 - Incorrectly Oriented (Facing West)']
         rows = ['', 'x-flipped']
 
         fig, axes = plt.subplots(2, 2, figsize=(8, 8), sharex=True, sharey=True)
