8f667993a2bdf593e3b5a0c3f7d6f3c0f0e2b6aa
Sherry <sherrylin42@gmail.com>
2021-09-22 20:26:41 -0400

    feat: model_specific folder as a function

--- moseq2_app/util.py
+++ moseq2_app/util.py
@@ -4,7 +4,10 @@ General utility functions.
 
 '''
 import pandas as pd
-from os.path import basename
+from os.path import basename, join, exists
+from os import listdir, mkdir
+from shutil import copy2
+from collections import defaultdict
 from moseq2_viz.util import read_yaml
 from moseq2_viz.scalars.util import scalars_to_dataframe
 from moseq2_viz.model.util import compute_behavioral_statistics
@@ -79,3 +82,37 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+def setup_model_folders(progress_paths):
+    """Create model-specific folders
+
+    Parameters
+    ----------
+    progress_paths (dict): dictionary of notebook progress paths.
+
+    Returns
+    -------
+    model_dict (dict): dictionary for model specific paths such as model_session_path, model_path, syll_info, syll_info_df and crowd_dir
+    """
+    # find all the models in the model master path
+    models = [file for file in listdir(progress_paths['model_master_path']) if file.endswith('.p')]
+    
+    # initialize model dictionary
+    model_dict = defaultdict(dict)
+
+    for model in models:
+        model_dir = join(progress_paths['model_master_path'], model.split('.')[0])
+        # enusre previous progress are not wiped
+        if exists(model_dir):
+            if exists(join(model_dir, model)):
+                pass
+            else:
+                copy2(join(progress_paths['model_master_path'], model), model_dir)
+        else:
+            mkdir(model_dir)
+            copy2(join(progress_paths['model_master_path'], model), model_dir)
+            print('Creating the model folder...')
+        model_dict[model]['model_session_path'] = model_dir
+        model_dict[model]['model_path'] = join(model_dir, model)
+    return model_dict
+
