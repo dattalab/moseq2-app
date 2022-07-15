029afdbf4923e3e99285111dd3653dbec3ba780a
Sherry <sherrylin42@gmail.com>
2022-07-15 16:35:29 -0400

    chore: change node size

--- moseq2_app/stat/view.py
+++ moseq2_app/stat/view.py
@@ -1013,7 +1013,8 @@ def set_node_colors_and_sizes(graph, usages, node_indices, difference_graph=Fals
     # node size is likely related to node diameters from https://towardsdatascience.com/customizing-networkx-graphs-f80b4e69bedf
     if difference_graph:
         node_color = {s: 'red' if usages[s] > 0 else 'blue' for s in node_indices}
-        node_size = {s: max(15., 10 + abs(usages[s] * 1000)) for s in node_indices}
+        node_size = {s: max(15., 15 + abs(usages[s] * 1000)) for s in node_indices}
+        
     else:
         node_color = {s: 'red' for s in node_indices}
         node_size = {s: max(15., abs(usages[s] * 1000)) for s in node_indices}
