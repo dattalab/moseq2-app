ad7212b7a30be43c5dd5060a2109f1528906ca7b
Sherry <sherrylin42@gmail.com>
2023-04-05 17:31:55 -0400

    chore: deactivating outdated tests

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -5,20 +5,20 @@ class TestInstallation(TestCase):
     def test_check_moseq2_extract(self):
         import moseq2_extract
 
-        assert moseq2_extract.__version__ == 'v1.1.2'
+        assert moseq2_extract.__version__ == 'v1.2.0'
 
     def test_check_moseq2_pca(self):
         import moseq2_pca
 
-        assert moseq2_pca.__version__ == 'v1.1.3'
+        assert moseq2_pca.__version__ == 'v1.2.0'
 
     def test_check_moseq2_model(self):
         import moseq2_model
 
-        assert moseq2_model.__version__ == 'v1.1.2'
+        assert moseq2_model.__version__ == 'v1.2.0'
 
 
     def test_check_moseq2_viz(self):
         import moseq2_viz
 
-        assert moseq2_viz.__version__ == 'v1.2.0'
+        assert moseq2_viz.__version__ == 'v1.3.0'
